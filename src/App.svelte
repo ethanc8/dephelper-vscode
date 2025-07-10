c88af7bd0001d651b0c027a46b374550945bcce3
ethanc8 <echaroenpitaks@gmail.com>
2025-07-10 13:09:20 -0500

    Add vscode-elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script>
 	import Data from "./components/data.svelte";
 	import './css/app.css';
+	import "@vscode-elements/elements/dist/vscode-button/index.js";
 </script>
 
 <p class="title">Hello, World!</p>
 <Data/>
+
+<vscode-button>Button</vscode-button>
