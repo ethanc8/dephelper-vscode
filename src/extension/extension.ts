83b018f3fc98aa016fd3b17c0a3a38ff2812f63b
ethanc8 <echaroenpitaks@gmail.com>
2025-07-10 14:27:12 -0500

    Show Svelte view in the sidebar

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,15 +1,15 @@
 import * as vscode from 'vscode';
 import { SveltePanel } from './panel';
+import { SvelteViewProvider } from './viewprovider';
 
 export function activate(context: vscode.ExtensionContext) {
-  const provider = new ColorsViewProvider(context.extensionUri);
+  const provider = new SvelteViewProvider(context.extensionUri);
 
   context.subscriptions.push(
-    vscode.window.registerWebviewViewProvider(ColorsViewProvider.viewType, provider));
+    vscode.window.registerWebviewViewProvider('dephelper-vscode.dephelper', provider));
   
-  context.subscriptions.push(vscode.commands.registerCommand('dephelper-vscode.helloWorld', () => {
+  context.subscriptions.push(vscode.commands.registerCommand('dephelper-vscode.openEditor', () => {
     SveltePanel.render("showPanel", "DepHelper", context.extensionUri);
-    provider.show();
 
     // Your setup should go here (things like posting things to the front end)
     // This is an example
@@ -19,89 +19,3 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
-
-// see https://github.com/microsoft/vscode-extension-samples/blob/main/webview-view-sample/src/extension.ts
-class ColorsViewProvider implements vscode.WebviewViewProvider {
-
-  public static readonly viewType = 'dephelper-vscode.dephelper';
-
-  private _view?: vscode.WebviewView;
-
-  constructor(
-    private readonly _extensionUri: vscode.Uri,
-  ) { }
-
-  public resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    _context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken,
-  ) {
-    this._view = webviewView;
-
-    webviewView.webview.options = {
-      // Allow scripts in the webview
-      enableScripts: true,
-
-      localResourceRoots: [
-        this._extensionUri
-      ]
-    };
-
-    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
-
-    // webviewView.webview.onDidReceiveMessage(data => {
-    //   switch (data.type) {
-    //     case 'colorSelected':
-    //       {
-    //         vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`#${data.value}`));
-    //         break;
-    //       }
-    //   }
-    // });
-  }
-
-  public show() {
-    if(this._view) {
-      this._view.show(true);
-    } else {
-      console.log("dephelper: this._view doesn't exist");
-    }
-  }
-
-  private _getHtmlForWebview(webview: vscode.Webview) {
-    // // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
-    // const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.js'));
-
-    // // Do the same for the stylesheet.
-    // const styleResetUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'reset.css'));
-    // const styleVSCodeUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'vscode.css'));
-    // const styleMainUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'main.css'));
-
-    // Use a nonce to only allow a specific script to be run.
-    const nonce = getNonce();
-
-    console.log("dephelper: got HTML!");
-
-    return `<!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-        <title>Cat Colors</title>
-      </head>
-      <body>
-        Hello world
-      </body>
-      </html>`;
-  }
-}
-
-function getNonce() {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
